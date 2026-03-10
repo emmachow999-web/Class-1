2102b6be8789ed3cc28908bdf2179352665c0ea5
Your Name <your.email@example.com>
2026-03-10 11:15:03 -0700

    updated stats

--- pokemon.py
+++ pokemon.py
@@ -9,16 +9,27 @@ if response.status_code != 200:
     
 else:
     response_obj = json.loads(response.content)
+    stats = response_obj["stats"]
 
     print("name is: ", response_obj["name"])
     print("height is: ", response_obj["height"])
     print("weight is: ", response_obj["weight"])
 
-stats = response_obj["stats"]
+    hp = 0
+    attack = 0
+    defense = 0
 
-hp = stats[0]["base_stat"]
-attack = stats[1]["base_stat"]
-defense = stats[2]["base_stat"]
-print("hp: ", hp)
-print("attack: ", attack)
-print("defense: ", defense)
+    for item in response_obj["stats"]:
+        stat_name = item["stat"]["name"]
+        stat_value = item["base_stat"]
+
+        if stat_name == "hp":
+            hp = stat_value
+        if stat_name == "attack":
+            attack = stat_value
+        if stat_name == "defense":
+            defense = stat_value
+    
+    print("hp: ", hp)
+    print("attack: ", attack)
+    print("defense", defense)
