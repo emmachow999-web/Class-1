43a0c37f414e68ebe75b037b475fd56b16747a27
emmachow999-web <emmachow999@gmail.com>
2026-04-24 19:05:29 -0700

    added new script and backend

--- hw-7/app.py
+++ hw-7/app.py
@@ -7,5 +7,18 @@ app = Flask(__name__)
 def index_entrance():
     return render_template('index.html'), 200
 
+@app.route('/backend', methods=['POST'])
+def backend():
+    my_input = json.loads(request.data)
+    
+    if isinstance(my_input["num1"],int) and isinstance(my_input["num2"],int):
+        return json.dumps({
+            "sum": my_input["num1"] + my_input["num2"]
+        })
+    else:
+        return json.dumps({
+            "error": "input isnt an integer"
+        })
+
 if __name__ == "__main__":
     app.run(debug=True)
--- hw-7/static/script.js
+++ hw-7/static/script.js
@@ -1,8 +1,54 @@
+var CLICK_COUNT = 0
+
+var CLICK_COUNT = 0
+
 document.getElementById('clickButton').addEventListener(
     'click',
     function() {
+        CLICK_COUNT += 1
         new_elmt = document.createElement('div')
-        new_elmt.innerHTML = "You clicked me!"
+        new_elmt.id = `CLICK_COUNTING_${CLICK_COUNT}`
+        new_elmt.innerHTML = `You clicked me ${CLICK_COUNT} times`
+        
         document.getElementById('display_area').appendChild(new_elmt)
+        
+        if (CLICK_COUNT > 3) {
+            count_to_reverse = CLICK_COUNT - 3
+            id_to_remove = `CLICK_COUNTING_${count_to_reverse}`
+            elmt_to_remove = document.getElementById(id_to_remove)
+            
+            elmt_to_remove.remove()
+        }
+   
+    }
+);
+
+
+document.getElementById('backendButton').addEventListener(
+    'click',
+    async function() {
+
+        num1_textbox =  document.getElementById("num1_textbox")
+        num2_textbox =  document.getElementById("num2_textbox")
+
+
+        response = await fetch("/backend", {
+            method: "POST",
+            body: JSON.stringify({
+                "num1": parseInt(num1_textbox.value),
+                "num2": parseInt(num2_textbox.value)
+            })
+        })
+
+        response_text = await response.text()
+        response_obj = JSON.parse(response_text)
+
+        if (response_obj["sum"] != null) {
+                document.getElementById("display_area_for_backend").innerHTML = response_obj["sum"]
+
+        }
+        else {
+            alert("Error: " + response_obj["error"])
+        }
     }
 );
